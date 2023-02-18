ca15377f7215b54b9062b277f60560da5fffac93
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-18 17:45:39 +0900

    feat: add popover + support system theme (#14)
    * feat: add Popover
    
    * feat: rewrite ThemeSelectButton with Popover
    
    * feat: add system theme option
    
    * refactor: refactor ThemeSelectButton
    
    * feat: add useMatchMedia
    
    * refactor: simplify "prefers-color-scheme" change event handler

--- packages/app/src/components/misc.tsx
+++ packages/app/src/components/misc.tsx
@@ -1,21 +1,64 @@
+import { Transition } from "@headlessui/react";
+import { tw } from "../styles/tw";
 import { cls, useThemeState } from "../utils/misc";
+import { Popover } from "./popover";
+
+const THEME_OPTIONS = [
+  ["system", "Use system theme"],
+  ["dark", "Dark theme"],
+  ["light", "Light theme"],
+] as const;
 
 export function ThemeSelectButton() {
   const [theme, setTheme] = useThemeState();
   return (
-    <button
-      className="flex items-center antd-btn antd-btn-ghost"
-      disabled={!theme}
-      onClick={() => {
-        setTheme(theme === "dark" ? "light" : "dark");
-      }}
-    >
-      <span
-        className={cls(
-          theme === "dark" ? "i-ri-sun-line" : "i-ri-moon-line",
-          "w-6 h-6"
-        )}
-      ></span>
-    </button>
+    <Popover
+      placement="bottom-end"
+      reference={({ props, open, setOpen }) => (
+        <button
+          className="flex items-center antd-btn antd-btn-ghost"
+          onClick={() => setOpen(!open)}
+          {...props}
+        >
+          <span className="!w-6 !h-6 light:i-ri-sun-line dark:i-ri-moon-line"></span>
+        </button>
+      )}
+      floating={({ props, open }) => (
+        <Transition
+          show={open}
+          className="transition duration-150 w-[230px]"
+          enterFrom="scale-80 opacity-0"
+          enterTo="scale-100 opacity-100"
+          leaveFrom="scale-100 opacity-100"
+          leaveTo="scale-80 opacity-0"
+          {...props}
+        >
+          {/* TODO: add arrow. improve shadow */}
+          <div className={tw.bg_colorBgElevated.shadow_lg.$}>
+            <ul className="flex flex-col gap-2 p-2">
+              {THEME_OPTIONS.map(([t, label]) => (
+                <li
+                  key={t}
+                  className={
+                    tw.antd_btn.antd_btn_text.p_1.px_2.flex.items_center.gap_2.$
+                  }
+                  onClick={() => {
+                    setTheme(t);
+                  }}
+                >
+                  <span
+                    className={cls(
+                      tw.i_ri_check_line.w_5.h_5.$,
+                      t !== theme && "invisible"
+                    )}
+                  ></span>
+                  {label}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Transition>
+      )}
+    />
   );
 }
