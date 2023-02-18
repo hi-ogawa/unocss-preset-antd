0394f98e817c8f960ae298b3165839ebf4fb23cc
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-02-18 17:04:23 +0900

    refactor: refactor ThemeSelectButton

--- packages/app/src/components/misc.tsx
+++ packages/app/src/components/misc.tsx
@@ -3,6 +3,12 @@ import { tw } from "../styles/tw";
 import { cls, useThemeState } from "../utils/misc";
 import { Popover } from "./popover";
 
+const THEME_OPTIONS = [
+  ["system", "Use system theme"],
+  ["dark", "Dark theme"],
+  ["light", "Light theme"],
+] as const;
+
 export function ThemeSelectButton() {
   const [theme, setTheme] = useThemeState();
   return (
@@ -14,74 +20,40 @@ export function ThemeSelectButton() {
           onClick={() => setOpen(!open)}
           {...props}
         >
-          <span
-            className={cls(
-              theme === "dark" ? "i-ri-moon-line" : "i-ri-sun-line",
-              "w-6 h-6"
-            )}
-          ></span>
+          <span className="!w-6 !h-6 light:i-ri-sun-line dark:i-ri-moon-line"></span>
         </button>
       )}
       floating={({ props, open }) => (
         <Transition
           show={open}
-          className="transition duration-150 w-[200px]"
+          className="transition duration-150 w-[230px]"
           enterFrom="scale-80 opacity-0"
           enterTo="scale-100 opacity-100"
           leaveFrom="scale-100 opacity-100"
           leaveTo="scale-80 opacity-0"
           {...props}
         >
-          <div className={tw.bg_colorBgElevated.shadow_2xl.$}>
+          {/* TODO: add arrow. improve shadow */}
+          <div className={tw.bg_colorBgElevated.shadow_lg.$}>
             <ul className="flex flex-col gap-2 p-2">
-              <li
-                className={
-                  tw.antd_btn.antd_btn_text.p_1.px_2.flex.items_center.gap_2.$
-                }
-                onClick={() => {
-                  setTheme("system");
-                }}
-              >
-                <span
-                  className={cls(
-                    tw.i_ri_check_line.w_5.h_5.$,
-                    theme !== "system" && "invisible"
-                  )}
-                ></span>
-                Use system theme
-              </li>
-              <li
-                className={
-                  tw.antd_btn.antd_btn_text.p_1.px_2.flex.items_center.gap_2.$
-                }
-                onClick={() => {
-                  setTheme("dark");
-                }}
-              >
-                <span
-                  className={cls(
-                    tw.i_ri_check_line.w_5.h_5.$,
-                    theme !== "dark" && "invisible"
-                  )}
-                ></span>
-                Dark theme
-              </li>
-              <li
-                className={
-                  tw.antd_btn.antd_btn_text.p_1.px_2.flex.items_center.gap_2.$
-                }
-                onClick={() => {
-                  setTheme("light");
-                }}
-              >
-                <span
-                  className={cls(
-                    tw.i_ri_check_line.w_5.h_5.$,
-                    theme !== "light" && "invisible"
-                  )}
-                ></span>
-                Light theme
-              </li>
+              {THEME_OPTIONS.map(([t, label]) => (
+                <li
+                  className={
+                    tw.antd_btn.antd_btn_text.p_1.px_2.flex.items_center.gap_2.$
+                  }
+                  onClick={() => {
+                    setTheme(t);
+                  }}
+                >
+                  <span
+                    className={cls(
+                      tw.i_ri_check_line.w_5.h_5.$,
+                      t !== theme && "invisible"
+                    )}
+                  ></span>
+                  {label}
+                </li>
+              ))}
             </ul>
           </div>
         </Transition>
