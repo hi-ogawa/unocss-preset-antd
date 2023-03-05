c948fa4495347bb3abc7fbf86876b240b5294679
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-03-05 18:55:46 +0900

    chore: fix tsup

--- packages/lib/tsup.config.ts
+++ packages/lib/tsup.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/reset.ts"],
+  entry: ["src/index.ts", "src/reset.ts", "src/reset.css"],
   format: ["esm", "cjs"],
   external: ["./reset.css"],
-  dts: true,
+  dts: {
+    entry: ["src/index.ts", "src/reset.ts"],
+  },
   splitting: false,
 });
