1020d0631cc469c3df1702f2e11c98d1430ac4c3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-03-13 00:16:21 +0900

    chore: update utils

--- packages/lib/src/misc.ts
+++
@@ -1,9 +0,0 @@
-// cheating type for convenience
-export function pickBy<K extends PropertyKey, V>(
-  o: Record<K, V>,
-  f: (v: V, k: K) => boolean
-): Record<K, V> {
-  return Object.fromEntries(
-    Object.entries<V>(o).filter(([k, v]) => f(v, k as any))
-  ) as any;
-}
