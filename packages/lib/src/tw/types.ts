d780931aa2aad2c5027a66c7b6bfafe01eb815a6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-03-25 19:56:21 +0900

    feat: add antd-btn-loading

--- packages/lib/src/tw/types.ts
+++ packages/lib/src/tw/types.ts
@@ -702,6 +702,7 @@ export type Theme_animation_keyframes =
   | `back_out_down`
   | `back_out_right`
   | `back_out_left`
+  | `spin_composable`
 ;
 
 export type Theme_animation_durations =
@@ -759,6 +760,7 @@ export type Theme_animation_counts =
   | `pulse_alt`
   | `bounce`
   | `bounce_alt`
+  | `spin_composable`
 ;
 
 export type Theme_media =
@@ -1708,6 +1710,7 @@ export type Shortcut =
   | `antd_tab`
   | `antd_tablist`
   | `antd_input`
+  | `antd_btn_loading`
   | `antd_btn_primary`
   | `antd_btn_default`
   | `antd_btn_ghost`
