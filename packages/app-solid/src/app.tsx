dc08048fbac1edd6e85e99a37760a7e60d45a88e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-05 22:50:56 +0900

    chore: comment about reactivety and controlled/uncontrolled input

--- packages/app-solid/src/app.tsx
+++ packages/app-solid/src/app.tsx
@@ -50,6 +50,8 @@ function ThemeSelect() {
       <SelectWrapper
         class="antd-input p-1 py-0.5 capitalize"
         options={["system", "dark", "light"]}
+        // very subtle nuance but it's not necessary to make `value` reactive by createSignal/createEffect
+        // since `select.value` can be left "uncontrolled" after the first rendering.
         value={__themeGet()}
         onChange={(v) => __themeSet(v)}
       />
