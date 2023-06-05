67cb977f4c80e9159b5c829f6951f22b9494ea46
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-05 21:38:40 +0900

    chore: tweak AppHeader

--- packages/app-solid/src/app.tsx
+++ packages/app-solid/src/app.tsx
@@ -15,9 +15,16 @@ export function App() {
 
 function AppHeader() {
   return (
-    <header class="flex p-2 px-4 shadow-md shadow-black/[0.05] dark:shadow-black/[0.7]">
+    <header class="flex items-center gap-2 p-2 px-4 shadow-md shadow-black/[0.05] dark:shadow-black/[0.7]">
       <h1 class="text-lg">Examples</h1>
       <span class="flex-1"></span>
+      <a
+        class="flex items-center antd-btn antd-btn-ghost"
+        href="https://github.com/hi-ogawa/unocss-preset-antd"
+        target="_blank"
+      >
+        <span class="i-ri-github-line w-6 h-6"></span>
+      </a>
       <ThemeSelect />
     </header>
   );
