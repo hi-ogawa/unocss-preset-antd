399559c056fcbbb7f0c7df7a056fce4cffa292da
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-09 13:31:25 +0900

    feat(app-solid): setup simple router (#46)

---
+++ packages/app-solid/src/components/router-utils.tsx
@@ -0,0 +1,53 @@
+import { createContextProvider } from "@solid-primitives/context";
+import { createHashHistory } from "history";
+import {
+  JSX,
+  createEffect,
+  createMemo,
+  createSignal,
+  onCleanup,
+  splitProps,
+  untrack,
+} from "solid-js";
+
+// create simple wrapper remix history for experimentation purpose
+// TODO: compare with solid-router
+
+// expose global history as context signal
+const __history = createHashHistory();
+
+const [HistoryProvider, useHistory] = createContextProvider(({}) => {
+  const [history, setHistory] = createSignal(__history, {
+    equals: () => false,
+  });
+
+  createEffect(() => {
+    const dispose = __history.listen(() => {
+      setHistory(__history);
+    });
+    onCleanup(() => dispose());
+  });
+
+  return history;
+}, undefined!);
+
+export { HistoryProvider, useHistory };
+
+export function Link(props: { to: string } & JSX.HTMLElementTags["a"]) {
+  const history = useHistory();
+  const href = createMemo(() => untrack(history).createHref(props.to));
+
+  return (
+    <a
+      href={href()}
+      data-active={history().location.pathname === props.to}
+      onClick={(e) => {
+        e.preventDefault();
+        untrack(history).push(props.to);
+      }}
+      {...splitProps(props, ["to"])[1]}
+    >
+      {props.children}
+    </a>
+  );
+}
