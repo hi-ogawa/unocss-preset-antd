520a988e32818ebfaa1c7bc4ce58639aba32779b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-06-09 13:20:07 +0900

    feat: setup routing

--- packages/app-solid/src/app.tsx
+++ packages/app-solid/src/app.tsx
@@ -1,13 +1,14 @@
-import type { Placement } from "@floating-ui/dom";
-import { range } from "@hiogawa/utils";
-import { Ref } from "@solid-primitives/refs";
-import { For, Index, Show, createSignal } from "solid-js";
-import { Modal } from "./components/modal";
-import { FloatingArrow, Popover } from "./components/popover";
+import {
+  Component,
+  For,
+  createComponent,
+  createMemo,
+  onMount,
+  untrack,
+} from "solid-js";
 import { HistoryProvider, Link, useHistory } from "./components/router-utils";
 import { ThemeSelect, changeLinkIconByTheme } from "./components/theme";
-import { Transition } from "./components/transition";
-import { cls } from "./components/utils";
+import * as stories from "./stories";
 
 export function App() {
   // TODO: provider chain utility?
@@ -18,30 +19,50 @@ export function App() {
   );
 }
 
-// TODO: glob import
-const APP_ROUTES = [{ path: "modal" }, { path: "popover" }];
+//
+// routes
+//
+
+// type check
+stories satisfies Record<string, Component>;
+
+const appRoutes = Object.entries(stories).map(([name, Component]) => ({
+  path: name.slice(4),
+  Component,
+}));
+
+function NotFound() {
+  const history = useHistory();
+  onMount(() => untrack(history).push("/Popover"));
+  return null;
+}
+
+//
+// layout
+//
 
 function AppInner() {
   changeLinkIconByTheme();
 
   const history = useHistory();
 
+  const outlet = createMemo(() => {
+    const pathname = history().location.pathname;
+    const found = appRoutes.find((route) => "/" + route.path === pathname);
+    return found?.Component ?? NotFound;
+  });
+
   return (
-    <div class="flex flex-col">
+    <div class="h-full flex flex-col">
       <AppHeader />
       <div class="flex-1 flex">
-        <div class="w-[250px] border-r p-2">
+        <div class="min-w-[150px] border-r p-2">
           <AppNavMenu />
         </div>
         <div class="flex-1 m-4">
           <div class="flex justify-center">
             <div class="flex flex-col gap-4 p-4">
-              <TestForm />
-              <TestTransition />
-              <TestModal />
-              <Show when={history().location.pathname === "/popover"}>
-                <TestPopover />
-              </Show>
+              {createComponent(outlet(), {})}
             </div>
           </div>
         </div>
@@ -71,7 +92,7 @@ function AppHeader() {
 function AppNavMenu() {
   return (
     <ul class="flex flex-col items-stretch gap-1.5">
-      <For each={APP_ROUTES}>
+      <For each={appRoutes}>
         {(item) => (
           <li class="flex">
             <Link
@@ -85,161 +106,4 @@ function AppNavMenu() {
       </For>
     </ul>
   );
-}
-
-function TestForm() {
-  return (
-    <div class="flex flex-col gap-3 p-2 border w-sm">
-      <h1 class="text-xl">Form</h1>
-      <label class="flex flex-col gap-1">
-        <span class="text-sm text-colorTextLabel">Username</span>
-        <input class="antd-input p-1" />
-      </label>
-      <button class="antd-btn antd-btn-primary p-1">Submit</button>
-    </div>
-  );
-}
-
-function TestTransition() {
-  const [show, setShow] = createSignal(false);
-
-  return (
-    <div class="flex flex-col gap-3 p-2 border w-sm">
-      <h1 class="text-xl">Transition</h1>
-      <button
-        class="antd-btn antd-btn-primary p-1"
-        onClick={() => setShow(!show())}
-      >
-        Toggle ({show() ? "on" : "off"})
-      </button>
-      <div class="h-[80px] flex items-center justify-center relative">
-        <div class="text-lg">Hello</div>
-        <Transition
-          show={show()}
-          class="absolute inset-0 antd-body flex items-center justify-center duration-1000"
-          enterFrom="opacity-0"
-          enterTo="opacity-80"
-          leaveFrom="opacity-80"
-          leaveTo="opacity-0"
-        >
-          <div class="antd-spin w-8 h-8"></div>
-        </Transition>
-      </div>
-    </div>
-  );
-}
-
-function TestModal() {
-  const [show, setShow] = createSignal(false);
-
-  return (
-    <div class="flex flex-col gap-3 p-2 border w-sm">
-      <h1 class="text-xl">Modal</h1>
-      <button
-        class="antd-btn antd-btn-primary p-1"
-        onClick={() => setShow(!show())}
-      >
-        Toggle ({show() ? "on" : "off"})
-      </button>
-      <Modal open={show()} setOpen={setShow}>
-        <div class="flex flex-col h-full p-3 gap-2">
-          <h3 class="text-lg">Modal Content Title</h3>
-          <div class="flex-1">Hello</div>
-          <div class="flex justify-end">
-            <button
-              class="antd-btn antd-btn-default px-2"
-              onClick={() => setShow(false)}
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </Modal>
-    </div>
-  );
-}
-
-function TestPopover() {
-  const placements = new Map<number, Placement>([
-    [1, "top-start"],
-    [2, "top"],
-    [3, "top-end"],
-    [5, "left-start"],
-    [10, "left"],
-    [15, "left-end"],
-    [9, "right-start"],
-    [14, "right"],
-    [19, "right-end"],
-    [21, "bottom-start"],
-    [22, "bottom"],
-    [23, "bottom-end"],
-  ]);
-
-  return (
-    <div class="flex flex-col gap-3 p-2 border w-sm">
-      <h1 class="text-xl">Popover</h1>
-      <div class="flex justify-center gap-2">
-        <div class="grid grid-cols-5 p-2 gap-2 w-sm">
-          <Index each={range(25).map((i) => placements.get(i))}>
-            {(item) => renderItem(item())}
-          </Index>
-        </div>
-      </div>
-    </div>
-  );
-
-  function renderItem(placement?: Placement) {
-    if (!placement) {
-      return <span />;
-    }
-
-    return (
-      <Popover
-        placement={placement}
-        reference={({ ctx }) => (
-          <button
-            class={cls(
-              "antd-btn antd-btn-default py-1 px-2 text-sm",
-              ctx.open() && "text-colorPrimaryActive border-colorPrimaryActive"
-            )}
-          >
-            {placement.replace("-", "\n")}
-          </button>
-        )}
-        floating={({ ctx, arrowCtx }) => (
-          <Transition
-            show={ctx.open()}
-            style={ctx.floatingStyle()}
-            class="transition duration-150"
-            enterFrom="scale-80 opacity-0"
-            enterTo="scale-100 opacity-100"
-            leaveFrom="scale-100 opacity-100"
-            leaveTo="scale-80 opacity-0"
-          >
-            <div class="antd-floating">
-              <Show when={ctx.placement()}>
-                {(placement) => (
-                  <Ref ref={arrowCtx.ref}>
-                    <FloatingArrow
-                      style={arrowCtx.style()}
-                      placement={placement()}
-                    />
-                  </Ref>
-                )}
-              </Show>
-              <div class="flex flex-col gap-2 px-3 py-2 w-[150px] text-sm">
-                <div>Content</div>
-                <button
-                  class="antd-btn antd-btn-default"
-                  onClick={() => ctx.setOpen(false)}
-                >
-                  Close
-                </button>
-              </div>
-            </div>
-          </Transition>
-        )}
-      />
-    );
-  }
 }
---
+++ packages/app-solid/src/stories.tsx
@@ -0,0 +1,165 @@
+import type { Placement } from "@floating-ui/dom";
+import { range } from "@hiogawa/utils";
+import { Ref } from "@solid-primitives/refs";
+import { Index, Show, createSignal } from "solid-js";
+import { Modal } from "./components/modal";
+import { FloatingArrow, Popover } from "./components/popover";
+import { Transition } from "./components/transition";
+import { cls } from "./components/utils";
+
+export function TestForm() {
+  return (
+    <div class="flex flex-col gap-3 p-2 border w-sm">
+      <h1 class="text-xl">Form</h1>
+      <label class="flex flex-col gap-1">
+        <span class="text-sm text-colorTextLabel">Username</span>
+        <input class="antd-input p-1" />
+      </label>
+      <button class="antd-btn antd-btn-primary p-1">Submit</button>
+    </div>
+  );
+}
+
+export function TestTransition() {
+  const [show, setShow] = createSignal(false);
+
+  return (
+    <div class="flex flex-col gap-3 p-2 border w-sm">
+      <h1 class="text-xl">Transition</h1>
+      <button
+        class="antd-btn antd-btn-primary p-1"
+        onClick={() => setShow(!show())}
+      >
+        Toggle ({show() ? "on" : "off"})
+      </button>
+      <div class="h-[80px] flex items-center justify-center relative">
+        <div class="text-lg">Hello</div>
+        <Transition
+          show={show()}
+          class="absolute inset-0 antd-body flex items-center justify-center duration-1000"
+          enterFrom="opacity-0"
+          enterTo="opacity-80"
+          leaveFrom="opacity-80"
+          leaveTo="opacity-0"
+        >
+          <div class="antd-spin w-8 h-8"></div>
+        </Transition>
+      </div>
+    </div>
+  );
+}
+
+export function TestModal() {
+  const [show, setShow] = createSignal(false);
+
+  return (
+    <div class="flex flex-col gap-3 p-2 border w-sm">
+      <h1 class="text-xl">Modal</h1>
+      <button
+        class="antd-btn antd-btn-primary p-1"
+        onClick={() => setShow(!show())}
+      >
+        Toggle ({show() ? "on" : "off"})
+      </button>
+      <Modal open={show()} setOpen={setShow}>
+        <div class="flex flex-col h-full p-3 gap-2">
+          <h3 class="text-lg">Modal Content Title</h3>
+          <div class="flex-1">Hello</div>
+          <div class="flex justify-end">
+            <button
+              class="antd-btn antd-btn-default px-2"
+              onClick={() => setShow(false)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export function TestPopover() {
+  const placements = new Map<number, Placement>([
+    [1, "top-start"],
+    [2, "top"],
+    [3, "top-end"],
+    [5, "left-start"],
+    [10, "left"],
+    [15, "left-end"],
+    [9, "right-start"],
+    [14, "right"],
+    [19, "right-end"],
+    [21, "bottom-start"],
+    [22, "bottom"],
+    [23, "bottom-end"],
+  ]);
+
+  return (
+    <div class="flex flex-col gap-3 p-2 border w-sm">
+      <h1 class="text-xl">Popover</h1>
+      <div class="flex justify-center gap-2">
+        <div class="grid grid-cols-5 p-2 gap-2 w-sm">
+          <Index each={range(25).map((i) => placements.get(i))}>
+            {(item) => renderItem(item())}
+          </Index>
+        </div>
+      </div>
+    </div>
+  );
+
+  function renderItem(placement?: Placement) {
+    if (!placement) {
+      return <span />;
+    }
+
+    return (
+      <Popover
+        placement={placement}
+        reference={({ ctx }) => (
+          <button
+            class={cls(
+              "antd-btn antd-btn-default py-1 px-2 text-sm",
+              ctx.open() && "text-colorPrimaryActive border-colorPrimaryActive"
+            )}
+          >
+            {placement.replace("-", "\n")}
+          </button>
+        )}
+        floating={({ ctx, arrowCtx }) => (
+          <Transition
+            show={ctx.open()}
+            style={ctx.floatingStyle()}
+            class="transition duration-150"
+            enterFrom="scale-80 opacity-0"
+            enterTo="scale-100 opacity-100"
+            leaveFrom="scale-100 opacity-100"
+            leaveTo="scale-80 opacity-0"
+          >
+            <div class="antd-floating">
+              <Show when={ctx.placement()}>
+                {(placement) => (
+                  <Ref ref={arrowCtx.ref}>
+                    <FloatingArrow
+                      style={arrowCtx.style()}
+                      placement={placement()}
+                    />
+                  </Ref>
+                )}
+              </Show>
+              <div class="flex flex-col gap-2 px-3 py-2 w-[150px] text-sm">
+                <div>Content</div>
+                <button
+                  class="antd-btn antd-btn-default"
+                  onClick={() => ctx.setOpen(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </Transition>
+        )}
+      />
+    );
+  }
+}
