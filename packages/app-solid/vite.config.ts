5d5e6e12fa9a1c359cce14173eaee0602cf5973c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-07-23 15:38:46 +0900

    chore: peer dep tweak (wip)

--- packages/app-solid/vite.config.ts
+++ packages/app-solid/vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
   },
   plugins: [
     unocss(),
-    // @ts-ignore confused vite peerDep?
     solid(),
     unocssDepHmrPlugin([require.resolve("@hiogawa/unocss-preset-antd")]),
     themeScriptPlugin({
