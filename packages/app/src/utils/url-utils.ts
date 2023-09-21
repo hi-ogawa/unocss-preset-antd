85c647fe8c87f0c57fce48be9476f73739b48d9b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-21 12:44:28 +0900

    chore: remove unused

--- packages/app/src/utils/url-utils.ts
+++ packages/app/src/utils/url-utils.ts
@@ -1,26 +1,5 @@
 import { useSearchParams } from "react-router-dom";
 
-type NextState<T> = T | ((prev: T) => T);
-
-type SetState<T> = (next: NextState<T>) => void;
-
-function resolveNext<T>(prev: () => T, next: NextState<T>): T {
-  return typeof next === "function" ? (next as any)(() => prev) : prev;
-}
-
-// simple state helper for string-key-value on URLSearchParams
-export function useUrlQuery<T extends Record<string, string>>(defaultValue: T) {
-  const [params, setParams] = useSearchParams(defaultValue);
-
-  const state = Object.fromEntries(params.entries()) as T;
-
-  const setState: SetState<T> = (next) => {
-    setParams(new URLSearchParams(resolveNext(() => state, next)));
-  };
-
-  return [state, setState] as const;
-}
-
 // TOOD: make TinyStoreAdapter?
 export function useUrlParam(key: string) {
   const [params, setParams] = useSearchParams();
