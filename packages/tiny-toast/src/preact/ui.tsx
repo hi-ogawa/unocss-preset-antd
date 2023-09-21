9577b4c7b4d182e5e5b22d6dba51f0694a0ace7d
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-21 23:14:14 +0900

    chore: inline style

--- packages/tiny-toast/src/preact/ui.tsx
+++ packages/tiny-toast/src/preact/ui.tsx
@@ -98,7 +98,11 @@ function ToastAnimation({
         pointerEvents: "auto",
       }),
     },
-    h("div", { class: "py-1" }, h(ToastItemComponent, { toast, item }))
+    h(
+      "div",
+      { style: istyle({ padding: "0.25rem 0" }) },
+      h(ToastItemComponent, { toast, item })
+    )
   );
 }
 
@@ -143,18 +147,41 @@ function ToastItemComponent({
           }),
         },
         [
-          // TODO: inline icon
           item.data.type === "success" &&
             h("span", {
-              class: "i-ri-checkbox-circle-fill text-green text-2xl",
+              style: istyle({
+                width: "1.5rem",
+                height: "1.5rem",
+                color: "#61d345",
+              }),
+              // https://remixicon.com/icon/checkbox-circle-fill
+              dangerouslySetInnerHTML: {
+                __html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11.0026 16L18.0737 8.92893L16.6595 7.51472L11.0026 13.1716L8.17421 10.3431L6.75999 11.7574L11.0026 16Z"></path></svg>`,
+              },
             }),
           item.data.type === "error" &&
             h("span", {
-              class: "i-ri-close-circle-fill text-red text-2xl",
+              style: istyle({
+                width: "1.5rem",
+                height: "1.5rem",
+                color: "#ff4b4b",
+              }),
+              // https://remixicon.com/icon/close-circle-fill
+              dangerouslySetInnerHTML: {
+                __html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 10.5858L9.17157 7.75736L7.75736 9.17157L10.5858 12L7.75736 14.8284L9.17157 16.2426L12 13.4142L14.8284 16.2426L16.2426 14.8284L13.4142 12L16.2426 9.17157L14.8284 7.75736L12 10.5858Z"></path></svg>`,
+              },
             }),
           item.data.type === "info" &&
             h("span", {
-              class: "i-ri-information-line text-blue text-2xl",
+              style: istyle({
+                width: "1.5rem",
+                height: "1.5rem",
+                color: "#1677ff",
+              }),
+              // https://remixicon.com/icon/information-fill
+              dangerouslySetInnerHTML: {
+                __html: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11 11V17H13V11H11ZM11 7V9H13V7H11Z"></path></svg>`,
+              },
             }),
           h(
             "div",
