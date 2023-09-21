a9fcfa157b61c311099c0bedb5b2b279757c6c83
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-09-21 10:20:47 +0900

    refactor(app): replace useUrlQuery with useUrlParam

--- packages/app/src/utils/url-utils.ts
+++ packages/app/src/utils/url-utils.ts
@@ -20,3 +20,22 @@ export function useUrlQuery<T extends Record<string, string>>(defaultValue: T) {
 
   return [state, setState] as const;
 }
+
+// TOOD: make TinyStoreAdapter?
+export function useUrlParam(key: string) {
+  const [params, setParams] = useSearchParams();
+
+  function get() {
+    return params.get(key) ?? undefined;
+  }
+
+  function set(v: string | undefined) {
+    setParams((prev) => {
+      prev = new URLSearchParams(prev);
+      typeof v === "string" ? prev.set(key, v) : prev.delete(key);
+      return prev;
+    });
+  }
+
+  return [get(), set] as const;
+}
